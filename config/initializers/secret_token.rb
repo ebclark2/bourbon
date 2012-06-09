40211e95bcf2b6c17042d0f17fca8652848c818f
Ed Clark <Ed@Eds-MacBook-Air.local>
2012-06-09 16:45:12 -0400

    Initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bourbon::Application.config.secret_token = '67b37cda7fbdb9c6cd83443cc135520d50b3c4f86e730723ec316f4aca6efe2868a8b3fc94be26154417f55cb88e858f3ce405288b58f8f6f72aca4e6de1a7aa'
