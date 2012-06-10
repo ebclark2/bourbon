b809f9b76c3f090e7e3c3097a4cca541a51105b1
Ed Clark <ebclark2@gmail.com>
2012-06-09 21:22:41 -0400

    Minor layout changes

--- app/models/whiskey.rb
+++ app/models/whiskey.rb
@@ -1,3 +1,8 @@
 class Whiskey < ActiveRecord::Base
   attr_accessible :category, :description, :distillery, :name
+
+  validates :category, :presence => true
+  validates :description, :presence =>true
+  validates :distillery, :presence =>true
+  validates :name, :presence =>true
 end
